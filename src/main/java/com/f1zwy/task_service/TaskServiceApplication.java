9687d61757b7fdd06bd3ee26030915f09315c7ed
YbGHFBK <142352084+YbGHFBK@users.noreply.github.com>
2025-06-01 12:46:52 +0300

    fix versions

--- src/main/java/com/f1zwy/task_service/TaskServiceApplication.java
+++ src/main/java/com/f1zwy/task_service/TaskServiceApplication.java
@@ -2,11 +2,10 @@ package com.f1zwy.task_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TaskServiceApplication {
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(TaskServiceApplication.class, args);
+		SpringApplication.run(TaskServiceApplication.class, args);
 	}
 }
